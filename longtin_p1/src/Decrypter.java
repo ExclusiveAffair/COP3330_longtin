f425860154e440316bf083f0087e42525af5f3db
Natalie <nlongtin2024@knights.ucf.edu>
2020-09-11 14:54:00 -0400

    Complete assignment
    Write the Encrypter and Decrypter classes with appropriate instance
    methods

--- longtin_p1/src/Decrypter.java
+++ longtin_p1/src/Decrypter.java
@@ -1,2 +1,31 @@
 public class Decrypter {
+    public String decrypt(String strToDecrypt) {
+        int[] digitArray=new int[4];
+        for(int i=0;i<4;i++) {
+            digitArray[i]=(int)(strToDecrypt.charAt(i)-'0');
+        }
+        swapDigits(digitArray,1,3);
+        swapDigits(digitArray,0,2);
+
+        for(int i=0;i<4;i++) {
+            digitArray[i]=addValueToDigit(digitArray[i],-7);
+        }
+
+        String decryptedResult="";
+        for(int i=0;i<4;i++) decryptedResult+=digitArray[i];
+
+        return decryptedResult;
+    }
+    public void swapDigits(int[] digitArray, int index1, int index2) {
+        int index1OriginalValue=digitArray[index1];
+
+        digitArray[index1]=digitArray[index2];
+        digitArray[index2]=index1OriginalValue;
+    }
+
+    public int addValueToDigit(int digit, int valueToAdd) {
+        int newDigit=digit+valueToAdd;
+        newDigit=(newDigit+10)%10;
+        return newDigit;
+    }
 }
--- longtin_p1/src/Encrypter.java
+++ longtin_p1/src/Encrypter.java
@@ -1,2 +1,30 @@
 public class Encrypter {
+    public String encrypt(String strToEncrypt) {
+        int[] digitArray=new int[4];
+        for(int i=0;i<4;i++) {
+            digitArray[i]=(int)(strToEncrypt.charAt(i)-'0');
+        }
+        for(int i=0;i<4;i++) {
+            digitArray[i]=addValueToDigit(digitArray[i],7);
+        }
+        swapDigits(digitArray,0,2);
+        swapDigits(digitArray,1,3);
+
+        String encryptedResult="";
+        for(int i=0;i<4;i++) encryptedResult+=digitArray[i];
+
+        return encryptedResult;
+    }
+    public void swapDigits(int[] digitArray, int index1, int index2) {
+        int index1OriginalValue=digitArray[index1];
+
+        digitArray[index1]=digitArray[index2];
+        digitArray[index2]=index1OriginalValue;
+    }
+
+    public int addValueToDigit(int digit, int valueToAdd) {
+        int newDigit=digit+valueToAdd;
+        newDigit=(newDigit+10)%10;
+        return newDigit;
+    }
 }
